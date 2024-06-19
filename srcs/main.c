57a186b8a1561e2372894618ff187868c2dd8fe2
StanX19 <tanshanchien@gmail.com>
2024-06-19 12:58:13 +0800

    str to argv fine tuning

--- srcs/main.c
+++ srcs/main.c
@@ -1,19 +1,37 @@
 #include "pipex.h"
 
-void	exec_command(const char *command, char *const*envp)
+// void	exec_command(const char *command, char *const*envp)
+// {
+// 	char			*file;
+// 	char			**argv;
+// 	t_stringstream	*ss;
+
+// 	ss = ss_create(command);
+// 	ss_read_argv(ss, &file);
+// 	// extract argv from command, can use ss
+// 	// find file's full name
+// 	execve(file, argv, envp);
+// }
+
+// int	main(int argc, char *const*argv, char *const*envp)
+// {
+
+// }
+
+int	main(void)
 {
-	char			*file;
 	char			**argv;
-	t_stringstream	*ss;
+	int				len;
 
-	ss = ss_create(command);
-	ss_read_argv(ss, &file)
-	// extract argv from command, can use ss
-	// find file's full name
-	execve(file, argv, envp);
-}
-
-int	main(int argc, char *const*argv, char *const*envp)
-{
-
+	argv = ft_str_to_argv("  \"hello mother\\\"   \" fucker", &len);
+	int	i;
+	
+	i = 0;
+	while (i < len)
+	{
+		printf("|%s|\n", argv[i]);
+		i++;
+	}
+	ft_free_2d((void **)argv, len);
+	return 0;
 }
