5fc31a3912b45cbf8ea3d051d02fce6f0a69a44b
Tan Shan Chien <tanshanchien@gmail.com>
2024-06-20 15:46:59 +0800

    ss recreate

--- srcs/main.c
+++ srcs/main.c
@@ -1,21 +1,34 @@
 #include "pipex.h"
 
-char	*get_executable(const char *name)
-{
+// char	*get_executable(const char *name)
+// {
 	
-}
+// }
 
-void	exec_command(const char *command, char *const*envp)
+// void	exec_command(const char *command, char *const*envp)
+// {
+// 	char			*file;
+// 	char			**argv;
+// 	int				argc;
+
+// 	argv = ft_str_to_argv(command, &argc);
+
+// }
+
+// int	main(int argc, char *const*argv, char *const*envp)
+// {
+
+// }
+
+int main(void)
 {
-	char			*file;
-	char			**argv;
-	int				argc;
+	char *str = "hello\\\\\\\\ mother *******";
+	int argc;
+	char **argv = ft_str_to_argv(str, &argc);
 
-	argv = ft_str_to_argv(command, &argc);
-
-}
-
-int	main(int argc, char *const*argv, char *const*envp)
-{
-
+	for(int i = 0; i < argc; i++)
+	{
+		printf("|%s|\n", argv[i]);
+	}
+	ft_free_2d((void **)argv, argc);
 }
